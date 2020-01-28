b02e57e57e0505562755858c312bfdca25a5f5ff
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 05:41:10 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,31 @@
 def starts_with_a_vowel?(word)
+  word ( /[aeiou]/)
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
+end
 
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
+def words_starting_with_un_and_ending_with_ing(text).scan (/\hun\w+ing\b/)
+end
 
 end
 
 def words_five_letters_long(text)
+  text.scan (/\b\w{5}\b/)
+end
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z]) == nil
+    return false 
+  else 
+    return true
+  end
 
 end
 
