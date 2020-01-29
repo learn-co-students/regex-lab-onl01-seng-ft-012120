210d5e4b0c095959268c2544725380dd9fce0cab
Diego Vasquez <botellongo69@outlook.com>
2020-01-29 19:44:07 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiou]/i) != nil ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  puts text.scan(/^uning$/i)
 end
 
 def words_five_letters_long(text)
