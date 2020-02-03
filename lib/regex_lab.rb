4b674f0f9374f60cccec04013b284a2a6f48348c
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 20:11:13 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiou]/i) != nil ? true : false
+  word.match(/^[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -12,7 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*[.?!]$/)  != nil ? true : false
+  text.match(/^[A-Z].*[.?!]$/)  ? true : false
 end
 
 def valid_phone_number?(phone)
