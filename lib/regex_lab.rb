3982d12d73bf61b67733f5c32f9b07e61834ff0d
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 19:29:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*[.,:?;]$/)  != nil ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  
 end
