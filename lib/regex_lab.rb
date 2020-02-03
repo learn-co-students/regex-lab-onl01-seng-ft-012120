16545035091623af562b5399c4d9887f9d54ce73
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 20:07:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def starts_with_a_vowel?(word)
   word.match(/^[aeiou]/i) != nil ? true : false
 end
@@ -11,9 +12,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*[.,:?;]$/)  != nil ? true : false
+  text.match(/^[A-Z].*[.?!]$/)  != nil ? true : false
 end
 
 def valid_phone_number?(phone)
-  
+  phone.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/) ? true : false
 end
